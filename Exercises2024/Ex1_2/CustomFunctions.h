dc920c3b51168bab8bfbf5a2f58e45a056c48743
qzahoor099 <qzahoor@cern.ch>
2024-11-25 21:05:39 +0000

    updated

--- Exercises2024/Ex1_2/CustomFunctions.h
+++ Exercises2024/Ex1_2/CustomFunctions.h
@@ -1,14 +1,14 @@
-#ifndef CUSTOMFUNCTIONS_H  //basically defining my header file using _H 
-#define CUSTOMFUNCTIONS_H
 
-//including the libraries
+#pragma once
+//including the libraries required 
 #include <vector>
-#include <utility> 
-#include <string>
-#include <iostream>
+#include<utility> 
+#include<string>
+#include<iostream>
+
 
 // Function to read data into a vector using the input file
-void readData(const std::string& filename, std::vector<std::pair<float, float>>& data);
+void readData(const std::string& filename, std::vector<std::pair<float, float>>&data);
 
 
 // implementing the temaplet inside the header file, using the printData fun, asking the user to 
@@ -16,7 +16,7 @@ void readData(const std::string& filename, std::vector<std::pair<float, float>>&
 //working for instruction 6 
 template <typename T>
 void printData(const T& data, int n) {
-    if (n > data.size()) {
+    if (n > data.size()) { 
         std::cerr << "Warning: Requested lines exceed total data points. Showing the first 5 points.\n";
         n = std::min((int)data.size(), 5);
         std::cout<<"the number of data lines selected"<<n<<std::endl;
@@ -26,24 +26,18 @@ void printData(const T& data, int n) {
     }
 }
 
-
-
 // function to calculate the magnitude of  a vector 
-std::vector<float> calculateMagnitudes(const std::vector<std::pair<float, float>>& data);
-void printMagnitude(const std::vector<float>& magnitudes);
-
+std::vector<float> calculateMagnitudes(const std::vector<std::pair<float, float>>&data);
+//void printMagnitude(const std::vector<float>& magnitudes);
+void printMagnitude(const std::vector<float>& magnitude);
 
 // writing magnitude to the file
-void writeToFile(const std::string& filename, const std::vector<float>& magnitudes);
+void writeToFile(const std::string& filename, const std::vector<float>& mangnitude);
 
 // fir into the straight line 
-void fitLine(const std::vector<std::pair<float, float>>& data, const std::string& filename);
-
+void fitLine(const std::vector<std::pair<float, float>>&data, const std::string& filename);
 // function to calculate x^y
-void calculatePower(const std::vector<std::pair<float, float>>& data, const std::string& filename);
-
+void calculatePower(const std::vector<std::pair<float,float>>&data,const std::string& filename);
 // calculating the chi function
 double calculateChiSquared(const std::vector<std::pair<float, float>>& data, double m, double c, const std::vector<float>& errors);
 
-#endif
-//end of the header file 
